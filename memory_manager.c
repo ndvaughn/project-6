f3d1dd3e9a95a7f84fb5b32bb1baf655b72bed66
nolanvaughn <nolanvaughn@MyVM.myguest.virtualbox.org>
2024-06-24 15:11:37 -0700

    initial

--- memory_manager.c
+++ memory_manager.c
@@ -7,7 +7,8 @@
 
 // TODO 1: Define your input parameter (pid - int) here
 // Then use module_param to pass them from insmod command line. (--Assignment 2)
-
+static int pid = 0;
+module_param(pid, int, 0644);
 
 struct task_struct *task = NULL;
 // Initialize memory statistics variables
@@ -27,33 +28,77 @@ static void parse_vma(void)
             mm = task->mm;
             // TODO 2: mm_struct to initialize the VMA_ITERATOR (-- Assignment 4)
             // Hint: Only one line of code is needed
-
+	    //struct vma_iterator vmi = {mm->vma_list, 0};
+	    VMA_ITERATOR(vmi, mm, 0);
 
             // TODO 3: Iterate through the VMA linked list with for_each_vma (-- Assignment 4)
             // Hint: Only one line of code is needed
+
+	    for_each_vma(vmi, vma){
 
 
                 // TODO 4: Iterate through each page of the VMA 
                 // declear "page" as unsigned long, start from vma->vm_start to vma->vm_end with PAGE_SIZE step 
                 // Hint: Only one line of code is needed
+		for (unsigned long int page = vma->vm_start; page < vma->vm_end; page += PAGE_SIZE){
 
 
                     // TODO 5: Use pgd_offset, p4d_offset, pud_offset, pmd_offset, pte_offset_map to get the page table entry
                     // Hint: Copy from Background Knowledge in the instruction
                     // Hint: change the address in the instruction to "page" variable you defined above
+		    pgd_t *pgd;
+		    p4d_t *p4d;
+		    pud_t *pud;
+		    pmd_t *pmd;
+		    pte_t *ptep, pte;
 
+		    pgd = pgd_offset(mm, page);
+		    if (pgd_none(*pgd) || pgd_bad(*pgd)) {
+			return;
+		    }
+
+		    p4d = p4d_offset(pgd, page);
+		    if (p4d_none(*p4d) || p4d_bad(*p4d)){
+			return;
+		    }
+
+		    pud = pud_offset(p4d, page);
+		    if (pud_none(*pud) || pud_bad(*pud)){
+			return;
+		    }
+
+		    pmd = pmd_offset(pud, page);
+		    if (pmd_none(*pmd) || pmd_bad(*pmd)){
+			return;
+		    }
+
+		   ptep = pte_offset_map(pmd, page);
+		   if (!ptep && !pte_page(*ptep)){
+			return;
+		   }
+
+		   pte = *ptep;
 
                     // TODO 6: use pte_none(pte) to check if the page table entry is valid
                     // Hint: one if statement here
-
+		   if (pte_none(pte)){
+			return;
+		   }
 
                     // TODO 7: use pte_present(pte) to check if the page is in memory, otherwise it is in swap
-
+		   if (pte_present(pte)){
+			total_rss++;
+		   }
+		   else{
+			total_swap++;
+		   }
 
                         // TODO 8: use pte_young(pte) to check if the page is actively used
                         // if it is actively used, update wss and clear the accessed bit by: "test_and_clear_bit(_PAGE_BIT_ACCESSED,(unsigned long *)ppte);"
-
-
+		    if (pte_young(pte)){
+		        total_wss++;
+			test_and_clear_bit(_PAGE_BIT_ACCESSED, page);
+		    }
                 }
             }
         }
